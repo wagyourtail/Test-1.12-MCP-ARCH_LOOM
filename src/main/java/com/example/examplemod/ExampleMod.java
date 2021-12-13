94dda0dbac889ca875c164d2d8b009b2ff0c6eba
Wagyourtail <wagyourtail@wagyourtail.xyz>
2021-12-13 10:49:49 -0700

    1.7.10

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,13 +1,12 @@
 package com.example.examplemod;
 
-import net.minecraft.client.Minecraft;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.init.Items;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.world.ExplosionEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -17,22 +16,26 @@ public class ExampleMod {
     public static final Logger LOGGER = LogManager.getLogger();
 
     public ExampleMod() {
+
+        // Most other events are fired on Forge's bus.
+        // If we want to use annotations to register event listeners,
+        // we need to register our object like this!
         MinecraftForge.EVENT_BUS.register(this);
     }
 
     @Mod.EventHandler
-    private void commonSetup(final FMLPreInitializationEvent event) {
+    private void commonSetup(final FMLPostInitializationEvent event) {
         LOGGER.info("Hello from common setup! This is *after* registries are done, so we can do this:");
-        LOGGER.info(String.format("Look, I found a %s!", Items.DIAMOND.getUnlocalizedName()));
+        LOGGER.info("Look, I found a {}!", Items.diamond.getUnlocalizedName());
     }
 
     @Mod.EventHandler
-    private void clientSetup(final FMLPostInitializationEvent event) {
-        LOGGER.info(String.format("Hey, we're on Minecraft version %s!", Minecraft.getMinecraft().getVersion()));
+    private void clientSetup(final FMLInitializationEvent event) {
+        LOGGER.info("Hey, we're on Minecraft version {}!", "1.7.10");
     }
 
     @SubscribeEvent
     public void kaboom(ExplosionEvent.Detonate event) {
-        LOGGER.info(String.format("Kaboom! Something just blew up in %s!", event.getWorld()));
+        LOGGER.info("Kaboom! Something just blew up in {}!", event.world);
     }
 }
